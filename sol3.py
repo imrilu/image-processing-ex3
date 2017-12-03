e4f0cb4e03f4923d5228b40d9c32ff07b8c90ba9
imrilu <imri.luzzattov@gmail.com>
2017-12-03 13:07:49 +0400

    changes

---
+++ sol3.py
@@ -0,0 +1,21 @@
+
+
+
+def read_image(filename, representation):
+    """
+    A method for reading an image from a path and loading in as gray or in color
+    :param filename: The path for the picture to be loaded
+    :param representation: The type of color the image will be load in. 1 for gray,
+    2 for color
+    :return: The loaded image
+    """
+    im = imread(filename)
+    if representation == 1:
+        # converting to gray
+        im = rgb2gray(im) / 255
+    else:
+        if representation == 2:
+            im = im.astype(np.float64)
+            # setting the image's matrix to be between 0 and 1
+            im = im / 255
+    return im
